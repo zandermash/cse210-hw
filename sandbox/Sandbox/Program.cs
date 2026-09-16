9be131538a89b02daaa35aece9e3b3fae644dc0c
Zander Marshall <zandermash@gmail.com>
2026-09-16 10:46:43 -0600

    notes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Bonjour tout le monde!");
+
+        // Input / Output
+        int x = 10;
+        Console.WriteLine($"x is {x}"); // this is how to do a formated string
+        //Console.Write();
+        Console.ReadLine(); // reads a line
     }
 }
