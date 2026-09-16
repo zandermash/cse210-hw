1ecdf50a4308386e1f7a5a2fa175f0a9591c9e85
Zander Marshall <zandermash@gmail.com>
2026-09-16 10:46:21 -0600

    Learning user input

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string firstName;
+        string lastName;
+        Console.Write("Please enter your first name: ");
+        firstName = Console.ReadLine();
+        Console.Write("Please enter your last name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"{firstName} {lastName}");
     }
 }
