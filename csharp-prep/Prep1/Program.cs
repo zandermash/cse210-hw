e0232845d0d0991e30ce33e27d8ab55af20e1967
Zander Marshall <zandermash@gmail.com>
2026-09-22 19:18:28 -0600

    Little change

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -13,6 +13,6 @@ class Program
         Console.Write("Please enter your last name:");
         lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
